738c5708af8869cc30589713bf719c1f5ee19798
Karen Burgos <karen@burgos.us>
2018-07-04 17:01:07 -0400

    Add React Routes

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
-
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
@@ -10,7 +10,30 @@ import Guilty from './components/Guilty';
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <Router>
+          <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/happy">Happy</Link>
+            </li>
+            <li>
+             <Link to="/guilty">Guilty</Link>
+            </li>
+            <li>
+             <Link to="/sleepy">Sleepy</Link>
+            </li>
+            </ul>
+
+         <Route path="/guilty" component={Guilty}/>
+         <Route path="/happy" component={Happy}/>
+         <Route path="/sleepy" component={Sleepy}/>
+
+            </div>
+</Router>
+
     )
   }
 }
